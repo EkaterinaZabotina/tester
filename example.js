96a720bc5c22b007c7cff16d21553615909c948a
Ekaterina <ekaterinaclassictravel@gmail.com>
2024-06-09 10:57:33 +0300

    added file task_score - hw 2-1-3

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-let strToNumb = "5"
+let strToNumb = "510"
 strToNumb = Number(strToNumb)
 console.log(typeof strToNumb)
 console.log(strToNumb)
---
+++ task_score.js
@@ -0,0 +1,9 @@
+let studentName = "Katya"
+let studentSurname = "Zabotina"
+
+let introModuleScore = 80
+let gitModuleScore = 90
+let jsModuleScore = 100
+
+let averageScore = (introModuleScore+gitModuleScore+jsModuleScore)/3
+console.log(`The average score for ${studentName} ${studentSurname} is ${averageScore}`)
