45749b017a7dc4a5cdb47dd87898f9f04d243b9f
Ekaterina <ekaterinaclassictravel@gmail.com>
2024-06-09 11:05:57 +0300

    delete file

--- task_score.js
+++
@@ -1,9 +0,0 @@
-let studentName = "Katya"
-let studentSurname = "Zabotina"
-
-let introModuleScore = 80
-let gitModuleScore = 90
-let jsModuleScore = 100
-
-let averageScore = (introModuleScore+gitModuleScore+jsModuleScore)/3
-console.log(`The average score for ${studentName} ${studentSurname} is ${averageScore}`)
